ec996b6a79f860aeb59565254dcb1eb4e7267232
Coopsy <f.farkas.erik998@gmail.com>
2021-11-10 00:02:11 +0100

    Napi adatok beolvasása

---
+++ Adatforrás/WeatherForecast.py
@@ -0,0 +1,5 @@
+import pandas as pd
+import numpy as np
+DailyDatas_columns=['Dátum','Napi Középhőmérséklet','Napi Maximumhőmérséklet','Napi Minimumhőmérséklet','Napi Csapadékösszeg','Napi Csapadékösszeg Fajtája','Napfénytartam Napi Összege','Globálsugárzás Napi Összege']
+DailyDatas = pd.read_csv(r'C:\Users\Coopsy\Desktop\afprepo\Projects\eghajlati_adatsor_1901-2020Budapest\napi_adatok\BP_d.txt', sep=';', skiprows=1, names=DailyDatas_columns)
+print(DailyDatas)
