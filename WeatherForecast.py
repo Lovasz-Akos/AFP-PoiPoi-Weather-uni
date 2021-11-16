90a243dd047fa57e5a19963239e8f7096ff53add
Coopsy <f.farkas.erik998@gmail.com>
2021-11-16 14:25:24 +0100

    DátumKód generáló függvény megírása

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -5,10 +5,22 @@ def readSource():
     DailyDatas_columns=['Dátum','Napi Középhőmérséklet','Napi Maximumhőmérséklet','Napi Minimumhőmérséklet','Napi Csapadékösszeg','Napi Csapadékösszeg Fajtája','Napfénytartam Napi Összege','Globálsugárzás Napi Összege']
     DailyDatas = pd.read_csv('Adatforrás/Budapest/napi_adatok/BP_d.txt', sep=';', skiprows=1, names=DailyDatas_columns)
     print("A fájl beolvasás sikeres")
+    
     return DailyDatas
+
+def generateDateCodes(DataFrame):
+    DateCodes=[0]*DataFrame.index.stop
+    i=0
+    for date in DataFrame['Dátum']:
+        DateCodes[i]= date.month*100 + date.day
+        i=i+1
+    return DateCodes
+        
     
 def main():
     DailyDatas = readSource()
+    DateCodes=pd.DataFrame(generateDateCodes(DailyDatas),columns=['DátumKód'])
+    print(DateCodes)
     
 main()
     
