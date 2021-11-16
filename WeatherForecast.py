81a3357fc60a24bab3e54b82be202bd10be74b50
Coopsy <f.farkas.erik998@gmail.com>
2021-11-16 14:28:57 +0100

    readSource függvény javítása
    Két sorral bővitettem az függvényt, így a benne található Dátumok már Date típusuak lesznek, ezáltal generateDateCodes is működni fog. Ezen kívűl kitöröltem egy oszlopot a DataFrame-ből, mivel az csak üres értékeket tartalmaz

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -5,7 +5,8 @@ def readSource():
     DailyDatas_columns=['Dátum','Napi Középhőmérséklet','Napi Maximumhőmérséklet','Napi Minimumhőmérséklet','Napi Csapadékösszeg','Napi Csapadékösszeg Fajtája','Napfénytartam Napi Összege','Globálsugárzás Napi Összege']
     DailyDatas = pd.read_csv('Adatforrás/Budapest/napi_adatok/BP_d.txt', sep=';', skiprows=1, names=DailyDatas_columns)
     print("A fájl beolvasás sikeres")
-    
+    DailyDatas['Dátum']=pd.to_datetime(DailyDatas['Dátum'])
+    DailyDatas=DailyDatas.drop(['Globálsugárzás Napi Összege'], axis=1)
     return DailyDatas
 
 def generateDateCodes(DataFrame):
