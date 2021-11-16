c7569829f3be071f90eeeee43cbe84c3eeb45eda
Coopsy <f.farkas.erik998@gmail.com>
2021-11-16 14:08:31 +0100

    Eljárások implementálása (bővebben commit desc)
    A fájl beolvasást egy readSource nevű függvénybe foglaltam, a fő programot pedig egy main nevű eljárásba tettem bele.

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -1,5 +1,14 @@
 import pandas as pd
 import numpy as np
-DailyDatas_columns=['Dátum','Napi Középhőmérséklet','Napi Maximumhőmérséklet','Napi Minimumhőmérséklet','Napi Csapadékösszeg','Napi Csapadékösszeg Fajtája','Napfénytartam Napi Összege','Globálsugárzás Napi Összege']
-DailyDatas = pd.read_csv('Adatforrás/Budapest/napi_adatok/BP_d.txt', sep=';', skiprows=1, names=DailyDatas_columns)
-print(DailyDatas)
+
+def readSource():
+    DailyDatas_columns=['Dátum','Napi Középhőmérséklet','Napi Maximumhőmérséklet','Napi Minimumhőmérséklet','Napi Csapadékösszeg','Napi Csapadékösszeg Fajtája','Napfénytartam Napi Összege','Globálsugárzás Napi Összege']
+    DailyDatas = pd.read_csv('Adatforrás/Budapest/napi_adatok/BP_d.txt', sep=';', skiprows=1, names=DailyDatas_columns)
+    print("A fájl beolvasás sikeres")
+    return DailyDatas
+    
+def main():
+    DailyDatas = readSource()
+    
+main()
+    
