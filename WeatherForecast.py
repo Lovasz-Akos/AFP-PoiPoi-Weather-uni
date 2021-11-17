dca6402123c27638d2236912ab9a6adfa1db777f
Coopsy <f.farkas.erik998@gmail.com>
2021-11-17 11:56:13 +0100

    Program betanítása, lineáris regresszióval

--- WeatherForecast.py
+++ WeatherForecast.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
 
 def readSource():
     DailyDatas_columns=['Dátum','Napi Középhőmérséklet','Napi Maximumhőmérséklet','Napi Minimumhőmérséklet','Napi Csapadékösszeg','Napi Csapadékösszeg Fajtája','Napfénytartam Napi Összege','Globálsugárzás Napi Összege']
@@ -28,12 +30,28 @@ def generateNKPredictors(DataFrame):
     NK_Predictors = NK_Predictors.drop(['Napfénytartam Napi Összege'],axis=1)
     return NK_Predictors    
     
+def weatherForecastWithLinearRegression(DataFrame):
+    X=pd.DataFrame(DataFrame['DátumKód'],columns=['DátumKód'])
+    y=pd.DataFrame(DataFrame['Napi Középhőmérséklet'],columns=['Napi Középhőmérséklet'])   
+    X_train, X_test, y_train, y_test= train_test_split(X, y, test_size=0.5)   
+    linearreg=LinearRegression()
+    linearreg.fit(X_train,y_train)
+    return linearreg
+    
+        
 def main():
     DailyDatas = readSource()
     DateCodes=pd.DataFrame(generateDateCodes(DailyDatas),columns=['DátumKód'])
     DailyDatas['DátumKód']=DateCodes
     NK_Predictors=generateNKPredictors(DailyDatas)
-    print(NK_Predictors)
+    
+    # 11. hó 18. nap DátumKódja: 1118
+    PredDate=[1118]
+    PredDate=pd.DataFrame(PredDate,columns=['DátumKód'])
+    linearreg=weatherForecastWithLinearRegression(NK_Predictors)
+    print(linearreg.predict(PredDate))
+    
+    
     
 main()
     
